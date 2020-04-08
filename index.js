d7585a68f5bb2722776d0ef77f516f1bf87201a6
HMJussani <hmjussani@gmail.com>
2020-04-08 12:54:06 -0300

    Index

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const routes = require('./src/routes');
+const cors = require('cors');
+const app = express();
+const porta= 3333;
+/*
+app.use(cors({
+    origin:'http://meuapp.com'
+}));*/
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+app.listen(porta, function(){
+    console.log(`Rodando na porta: ${porta}`);    
+});
+
+
+
+
+
