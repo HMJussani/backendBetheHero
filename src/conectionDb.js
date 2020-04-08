add554e822197cf65baaa199c0dd64b183ee2946
HMJussani <hmjussani@gmail.com>
2020-04-08 12:35:50 -0300

    First commit

---
+++ src/conectionDb.js
@@ -0,0 +1,9 @@
+const conection = require('knex')({
+    client: 'sqlite3',
+    connection: {
+      filename: '../database/dataBase.sqlite3'
+    }, 
+    useNullAsDefault:true 
+  });
+
+module.exports = conection;
